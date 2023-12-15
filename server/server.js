cd953383668cb97a42bc63bb6c9053528739b7f8
Matthew Cherry <matiwisnia16@gmail.com>
2023-12-15 22:57:06 +0100

    Add mongodb database connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', (request, response) => {
-    response.json({
-        message: 'The index page'
-    });
-});
+app.use(express.json());
 
+app.use((request, response, next) => {
+    console.log(request.path, request.method);
+    next();
+})
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
 app.listen(process.env.PORT, () => {
-    console.log('listening on port ' + process.env.PORT);
+            console.log('Server is running on port ' + process.env.PORT);
 });
+    })
+    .catch((error) => {
+        console.log(error)
+    });
